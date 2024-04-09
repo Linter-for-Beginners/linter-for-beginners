2b939f3ef7472b94f499ed3d129425b209f6c140
Saisyc <saisyc@outlook.com>
2024-04-09 12:47:47 +0800

    example

--- LFB/LinBee/LinBee23/LinBee23/out/artifacts/LinBee23_jar/unnamed.c
+++
@@ -1,4 +0,0 @@
-int main() {
-	printf("%d %d %lf", 6, 2.345, 90.0);
-	return 0.0;
-}
--- LFB/LinBee/LinBee23/LinBee23/source.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int a, b, c;
-int main() {
-	scanf("%d%d", &a, b);
-	c = a * b - (a == b);
-	printf("%d", c);
-	return c, 0;
-}
-
